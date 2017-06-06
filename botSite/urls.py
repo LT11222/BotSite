4f2a284f96a075b1a6e3b5e2b63074d1769eb81a
Luke <luke11222@gmail.com>
2017-06-05 21:02:28 -0400

    Abstracted database further with templates, beautified a little with bootstrap

--- botSite/urls.py
+++ botSite/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="homepage.html"), name="homepage"),
+    url(r'^$', TemplateView.as_view(template_name="base_homepage.html"), name="homepage"),
     url(r'^database/', include('database.urls'), name="database")
 ]
